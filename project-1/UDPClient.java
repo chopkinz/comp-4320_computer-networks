9de3c2a0b4a6bf1e176252a46c1833395222cffc
Chase <chasehop334@gmail.com>
2022-07-21 13:06:46 -0500

    implemented basic functionality and test file - just need to run scripts and make minor edits before submitting.

---
+++ project-1/UDPClient.java
@@ -0,0 +1,103 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.awt.*;
+/* This program is a UDP client that sends a message to a server.
+*/
+
+class UDPClient {
+
+	public static void main(String args[]) throws Exception {
+
+		// Variable initializations
+		DatagramSocket clientSocket = new DatagramSocket();
+		InetAddress serverIP = InetAddress.getByName("localhost");
+		int serverPort = 9090;
+		String httpRequest = "GET TestFile.html HTTP/1.0";
+
+		// Send packet to server
+		System.out.println("-------------------------Sending Data to Server-------------------------");
+		byte[] dataOut = httpRequest.getBytes();
+		DatagramPacket packetOut = new DatagramPacket(
+				dataOut, dataOut.length, serverIP, serverPort);
+		clientSocket.send(packetOut);
+		System.out.println("Complete");
+
+		// Receive packet from server
+		System.out.println("-----------------------Receiving Data from Server-----------------------");
+		byte[] dataIn = new byte[256];
+		DatagramPacket packetIn = new DatagramPacket(dataIn, dataIn.length);
+
+		ArrayList<UDPPacket> received = new ArrayList<>();
+		boolean finished = false;
+		int packetNum = 0;
+
+		while (!finished) {
+			clientSocket.receive(packetIn);
+			UDPPacket dataReceived = UDPPacket.makePacket(packetIn);
+			packetNum++;
+			if (dataReceived.getPacketData()[0] == '\0') {
+				finished = true;
+			} else {
+				received.add(dataReceived);
+				System.out.println("Received packetNum: " + packetNum);
+			}
+		}
+
+		clientSocket.close();
+
+		System.out.println("------------------------Running Gremlin Function------------------------");
+		String gremlinProb = "0.0";
+		if (args.length != 0) {
+			gremlinProb = args[0];
+		} else {
+			System.out.println("There were no command line argumants for gremlin function");
+		}
+
+		for (UDPPacket packet : received) {
+			gremlin(gremlinProb, packet);
+		}
+
+		errorDetection(received);
+
+		byte[] reassembledFile = UDPPacket.reassemble(received);
+		String reassembledFileString = new String(reassembledFile);
+		System.out.println("\nfile recieved from server:\n" + reassembledFileString);
+	}
+
+	private static void errorDetection(ArrayList<UDPPacket> packetList) {
+		for (UDPPacket packet : packetList) {
+			String checksumHeaderValue = packet.getHeaderValue(UDPPacket.HEADER_VALUES.CHECKSUM);
+			Short checkSum = Short.parseShort(checksumHeaderValue);
+			byte[] data = packet.getPacketData();
+			short calculatedCheckSum = UDPPacket.calculateChecksum(data);
+			if (!checkSum.equals(calculatedCheckSum)) {
+				String segmentHeaderValue = packet.getHeaderValue(UDPPacket.HEADER_VALUES.SEGMENT_NUM);
+				System.out.println("Error detected in UDPPacket Number: " + segmentHeaderValue);
+			}
+		}
+	}
+
+	private static void gremlin(String probability, UDPPacket packet) {
+		Random rand = new Random();
+		double damageProb = rand.nextDouble();
+		double flipProb = rand.nextDouble();
+		int bytesToChange;
+
+		if (flipProb <= 0.5) {
+			bytesToChange = 1;
+		} else if (flipProb <= 0.8) {
+			bytesToChange = 2;
+		} else {
+			bytesToChange = 3;
+		}
+
+		if (Double.parseDouble(probability) >= damageProb) {
+			for (int i = 0; i < bytesToChange; i++) {
+				byte[] data = packet.getPacketData();
+				int byteNum = rand.nextInt(packet.getPacketSize());
+				data[byteNum] = (byte) ~data[byteNum];
+			}
+		}
+	}
+}
---
+++ project-1/UDPServer.java
@@ -0,0 +1,65 @@
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+class UDPServer {
+	public static void main(String args[]) throws Exception {
+
+		// Create socket at port 9090
+		DatagramSocket serverSocket = new DatagramSocket(9090);
+
+		// Create byte arrays
+		byte[] dataIn = new byte[256];
+		String nullByte = "\0";
+
+		while (true) {
+
+			// Get packet sent from client
+			DatagramPacket packetIn = new DatagramPacket(dataIn, dataIn.length);
+			serverSocket.receive(packetIn);
+
+			// Get client IP address, port number, and HTTP request
+			InetAddress clientIP = packetIn.getAddress();
+			int clientPort = packetIn.getPort();
+			String clientGetRequest = new String(packetIn.getData());
+
+			// Get the name of the file requested by the client
+			String fileName = clientGetRequest.substring(
+					clientGetRequest.indexOf(" ") + 1, clientGetRequest.lastIndexOf(" "));
+			BufferedReader fileIn = new BufferedReader(new FileReader(fileName));
+			StringBuilder fileDataContents = new StringBuilder();
+
+			String line = fileIn.readLine();
+			System.out.println("line: " + line);
+			while (line != null) {
+				System.out.println(line);
+				fileDataContents.append(line);
+				line = fileIn.readLine();
+			}
+			fileIn.close();
+
+			String httpHeader = "HTTP/1.0 TestFile.html Follows\r\n"
+					+ "Content-Type: text/plain\r\n"
+					+ "Content-Length: " + fileDataContents.length() + "\r\n"
+					+ "\r\n" + fileDataContents;
+
+			ArrayList<UDPPacket> packetList = UDPPacket.segment(httpHeader.getBytes()); // segments file into packets
+			System.out.println("List of segmented packets is " + packetList.size() + " packets long");
+
+			for (UDPPacket packet : packetList) {
+				DatagramPacket sendPacket = packet.getDatagramPacket(clientIP, clientPort);
+				serverSocket.send(sendPacket);
+			}
+
+			// Notify the client that all data has been sent via a null character
+			System.out.println("Sending null character");
+			ArrayList<UDPPacket> nullPacket = UDPPacket.segment(nullByte.getBytes());
+			DatagramPacket nullDatagram = nullPacket.get(0).getDatagramPacket(clientIP, clientPort);
+			serverSocket.send(nullDatagram);
+			System.out.print("Sent");
+		}
+
+	}
+
+}
